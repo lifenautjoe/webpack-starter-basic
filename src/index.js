0795454312281c198a72f2d95615b7b057d5d045
Joel Hernandez <involvmnt@gmail.com>
2017-10-07 10:36:07 +0200

    chore: update demo website

--- src/index.js
+++ src/index.js
@@ -10,7 +10,7 @@ document.addEventListener("DOMContentLoaded", () => {
     const pluginsTriggerElement = document.getElementById('plugins-trigger');
     const pluginsElement = document.getElementById('plugins');
 
-    const pluginsVisibleClass = "splash-overview-plugins__list";
+    const pluginsVisibleClass = "splash-overview-plugins__list--visible";
 
     pluginsTriggerElement.onclick = () => {
         pluginsElement.classList.toggle(pluginsVisibleClass);
