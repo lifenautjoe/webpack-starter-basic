add18ab891e3a9e3fca1500285316542ccd1fa55
Joel Hernandez <lifenautjoe@gmail.com>
2018-03-24 14:36:09 +0100

    ⬆️ update babel preset

--- webpack.prod.js
+++ webpack.prod.js
@@ -23,7 +23,7 @@ module.exports = {
                 loader: 'babel-loader',
 
                 options: {
-                    presets: ['es2015']
+                    presets: ['env']
                 }
             },
             {
