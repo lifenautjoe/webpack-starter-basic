291fa375e905c61cc5d683c42eaa8ac1a859b18a
Cândido <candidosg@gmail.com>
2019-04-04 09:23:23 -0300

    add purgecss and loader to icons - svg, woff,  woff2, tff , eot

--- postcss.config.js
+++ postcss.config.js
@@ -3,18 +3,28 @@
 * These plugins can lint your CSS, support variables and mixins, transpile future CSS syntax, inline images, and more.
 * https://github.com/postcss/postcss
 */
+
+const purgecss = require('@fullhuman/postcss-purgecss');
+const autoprefixer = require('autoprefixer');
+
 module.exports = {
     plugins: [
+        /*
+        * Remove unused CSS
+        */
+        purgecss({
+            content: ['./**/*.html']
+        }),
         /*
         * Adds vendor prefixes to css attributes
         * https://github.com/postcss/autoprefixer
         */
-        require('autoprefixer')({
+       autoprefixer({
             /* It should add vendor prefixes for the last 2 versions of all browsers, meaning old prefixes such as
             * -webkit-border-radius: 5px; that the latest browsers support as border-radius won't be added.
             * https://github.com/ai/browserslist#queries
             */
             browsers: 'last 2 versions'
-        })
+        }),
     ]
 };
